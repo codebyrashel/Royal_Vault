bcb50a34495b45787cb4728ca41a97e28e84564e
Rashel H. <codebyrashel@gmail.com>
2026-01-15 00:02:29 +0600

    Implemented Vue frontend with authentication flow
    - Added Vue 3 frontend
    - Implemented login and registration UI
    - Integrated JWT token handling
    - Connected frontend to backend API
    - Added protected dashboard view

---
+++ client/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Dashboard from "../views/Dashboard.vue";
+
+const routes = [
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+  { path: "/dashboard", component: Dashboard },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <button @click="loadProfile">Load Profile</button>
+    <pre>{{ user }}</pre>
+  </div>
+</template>
+
+<script>
+import api from "../api/api";
+
+export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
+  methods: {
+    async loadProfile() {
+      const res = await api.get("/vault/me");
+      this.user = res.data;
+    },
+  },
+};
+</script>
